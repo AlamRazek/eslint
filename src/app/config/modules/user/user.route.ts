87e1fa21c127fa27d8e8e7ba5d3eeb0366e33b28
AlamRazek <ohidul933@gmail.com>
2024-05-29 04:00:47 +0100

    catchAsync function created

--- src/app/config/modules/user/user.route.ts
+++ src/app/config/modules/user/user.route.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
-router.post('/create-student', UserControllers.createStudent);
+const military = (req: Request, res: Response, next: NextFunction) => {
+  console.log('i am a military');
+};
+
+router.post('/create-student', military, UserControllers.createStudent);
 
 export const UsersRoutes = router;
