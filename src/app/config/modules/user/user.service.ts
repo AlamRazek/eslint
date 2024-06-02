0260e486f0d59006f32d2a68f790b479f5211cdc
AlamRazek <ohidul933@gmail.com>
2024-06-02 03:13:30 +0100

    update router updated

--- src/app/config/modules/user/user.service.ts
+++ src/app/config/modules/user/user.service.ts
@@ -1,12 +1,13 @@
 import config from '../..';
 import { TAcademicSemseter } from '../academicSemester/academicSemester.interface';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { Student } from '../student.modul';
 import { TStudent } from '../student/student.interface';
 import { TUser } from './user.interface';
 
 import { User } from './user.model';
 
-const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user
   const userData: Partial<TUser> = {};
 
@@ -17,20 +18,25 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   userData.role = 'student';
 
   // year semester 4digit number
-  const generalStudentId = (payload: TAcademicSemseter) => {};
+  const generateStudentId = (payload: TAcademicSemseter) => {};
+
+  // find academic semester info
+  const admissionSemester = await AcademicSemester.findById(
+    payload.admissionSemester,
+  );
 
   // manual generate id
-  userData.id = generateStudentId();
+  userData.id = generateStudentId(admissionSemester);
 
   // create a user model
   const newUser = await User.create(userData); // built in static method
 
   //create a student
   if (Object.keys(newUser).length) {
-    studentData.id = newUser.id;
-    studentData.user = newUser._id; // reference id
+    payload.id = newUser.id;
+    payload.user = newUser._id; // reference id
 
-    const newStudent = await Student.create(studentData);
+    const newStudent = await Student.create(payload);
     return newStudent;
   }
 };
---
+++ src/app/config/modules/user/user.utils.ts
@@ -0,0 +1,4 @@
+// year semester 4digit number
+export const generateStudentId = (payload: TAcademicSemseter) => {
+  const currentId = (0).toString().padStart(4, '0');
+};
