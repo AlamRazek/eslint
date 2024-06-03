0ba06bf547cb85033a9021e41a8161bc936a0d29
AlamRazek <ohidul933@gmail.com>
2024-06-03 23:11:32 +0100

    academy facylty service updated

---
+++ src/app/config/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,35 @@
+import { TAcademicSemseter } from '../academicSemester/academicSemester.interface';
+import { TAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFaculty } from './academicFaculty.model';
+
+const createAcademicFacultyIntiDb = async (payload: TAcademicFaculty) => {
+  const result = await AcademicFaculty.create(payload);
+  return result;
+};
+
+const getAllAcademicFacultyFromDB = async () => {
+  const result = await AcademicFaculty.find();
+  return result;
+};
+
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFaculty.findById(id);
+  return result;
+};
+
+const updateAcademicFacultyIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicSemseter>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
+export const AcademicSemesterServices = {
+  createAcademicFacultyIntiDb,
+  getAllAcademicFacultyFromDB,
+  getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
+};
