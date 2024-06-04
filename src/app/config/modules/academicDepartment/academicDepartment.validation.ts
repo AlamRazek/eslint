96a209d89426d873c5de9fefbdf8533a0f6c7959
AlamRazek <ohidul933@gmail.com>
2024-06-04 03:09:31 +0100

    academy  name validation done

--- src/app/config/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/config/modules/academicDepartment/academicDepartment.validation.ts
@@ -4,6 +4,11 @@ const createAcademicFacultyValidationSchema = z.object({
   body: z.object({
     name: z.string({
       invalid_type_error: 'Academic Department must be string',
+      required_error: 'Name is required',
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Department must be string',
+      required_error: 'Faculty is required',
     }),
   }),
 });
