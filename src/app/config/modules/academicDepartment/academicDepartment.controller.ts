34813fc328586e11635e688d2bb22ad978d356cb
AlamRazek <ohidul933@gmail.com>
2024-06-04 03:32:32 +0100

    academy department  controller is created

---
+++ src/app/config/modules/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,55 @@
+import sendResponse from '../../../utils/sendResponse';
+import httpStatus from 'http-status';
+import catchAsync from '../../../utils/catchAsync';
+
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.createAcademicFacultyIntiDb(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty created successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultyFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculties are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const { FacultyId } = req.params;
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyFromDB(FacultyId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is retrieved successfully',
+    data: result,
+  });
+});
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params;
+  const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(
+    facultyId,
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is updated successfully',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {};
