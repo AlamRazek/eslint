acf1c0bafed390be68ceef3417c19673f7a69289
AlamRazek <ohidul933@gmail.com>
2024-06-04 03:27:33 +0100

    academy department  service is created

---
+++ src/app/config/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,31 @@
+const createAcademicFacultyIntiDb = async (payload: TAcademicFaculty) => {
+  const result = await AcademicFaculty.create(payload);
+  return result;
+};
+
+const getAllAcademicFacultyFromDB = async () => {
+  const result = await AcademicFaculty.find();
+  return result;
+};
+
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFaculty.findById(id);
+  return result;
+};
+
+const updateAcademicFacultyIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicSemseter>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
+export const AcademicFacultyServices = {
+  createAcademicFacultyIntiDb,
+  getAllAcademicFacultyFromDB,
+  getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
+};
