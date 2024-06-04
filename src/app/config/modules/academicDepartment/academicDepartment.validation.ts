dd698ad1b20e7a4cf2d3b3790bcb586921772afc
AlamRazek <ohidul933@gmail.com>
2024-06-04 03:12:31 +0100

    academy department  validation updated

--- src/app/config/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/config/modules/academicDepartment/academicDepartment.validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const createAcademicFacultyValidationSchema = z.object({
+const createAcademicDepartmentValidationSchema = z.object({
   body: z.object({
     name: z.string({
       invalid_type_error: 'Academic Department must be string',
@@ -12,15 +12,24 @@ const createAcademicFacultyValidationSchema = z.object({
     }),
   }),
 });
-const updateAcademicFacultyValidationSchema = z.object({
+const updateAcademicDepartmentValidationSchema = z.object({
   body: z.object({
-    name: z.string({
-      invalid_type_error: 'Academic Department must be string',
-    }),
+    name: z
+      .string({
+        invalid_type_error: 'Academic Department must be string',
+        required_error: 'Name is required',
+      })
+      .optional(),
+    academicFaculty: z
+      .string({
+        invalid_type_error: 'Academic Department must be string',
+        required_error: 'Faculty is required',
+      })
+      .optional(),
   }),
 });
 
-export const AcademicFacultyValidation = {
-  createAcademicFacultyValidationSchema,
-  updateAcademicFacultyValidationSchema,
+export const AcademicDepartmentValidation = {
+  createAcademicDepartmentValidationSchema,
+  updateAcademicDepartmentValidationSchema,
 };
