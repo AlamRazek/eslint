227c3a37b34ec5aec7925e901e794749bda06a03
AlamRazek <ohidul933@gmail.com>
2024-06-05 03:44:39 +0100

    user service session created

--- src/app/config/modules/user/user.service.ts
+++ src/app/config/modules/user/user.service.ts
@@ -7,6 +7,9 @@ import { TUser } from './user.interface';
 
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import mongoose from 'mongoose';
+import AppError from '../../../errors/AppError';
+import httpStatus from 'http-status';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user
@@ -23,20 +26,36 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   );
 
-  // manual generate id
-  userData.id = await generateStudentId(admissionSemester as TAcademicSemseter);
+  const session = await mongoose.startSession();
 
-  // create a user model
-  const newUser = await User.create(userData); // built in static method
+  try {
+    session.startTransaction();
+    // set generate id
+    userData.id = await generateStudentId(
+      admissionSemester as TAcademicSemseter,
+    );
 
-  //create a student
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id;
-    payload.user = newUser._id; // reference id
+    // create a user model
+    const newUser = await User.create([userData], { session }); // built in static method
 
-    const newStudent = await Student.create(payload);
+    //create a student
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+    // set id
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; // reference id
+
+    const newStudent = await Student.create([payload], { session });
+
+    if (!newStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
+    }
+    await session.commitTransaction();
+    await session.endSession();
+
     return newStudent;
-  }
+  } catch (err) {}
 };
 
 export const USerServices = {
