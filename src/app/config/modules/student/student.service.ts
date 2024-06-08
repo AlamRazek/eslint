4b174df0594d09d6de12517c84bf37b49b71fd00
AlamRazek <ohidul933@gmail.com>
2024-06-08 20:59:39 +0100

    student service ...

--- src/app/config/modules/student/student.service.ts
+++ src/app/config/modules/student/student.service.ts
@@ -37,21 +37,24 @@ const updateStudentIntoDb = async (id: string, payload: Partial<TStudent>) => {
 
   if (name && Object.keys(name).length) {
     for (const [key, value] of Object.entries(name)) {
-      modifiedUpdatedData[`$name${key}`] = value;
+      modifiedUpdatedData[`name.${key}`] = value;
     }
   }
   if (guardian && Object.keys(guardian).length) {
     for (const [key, value] of Object.entries(guardian)) {
-      modifiedUpdatedData[`$guardian${key}`] = value;
+      modifiedUpdatedData[`guardian.${key}`] = value;
     }
   }
   if (localGuardian && Object.keys(localGuardian).length) {
     for (const [key, value] of Object.entries(localGuardian)) {
-      modifiedUpdatedData[`$localGuardian${key}`] = value;
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
     }
   }
 
-  const result = await Student.findOne({ id }, payload);
+  const result = await Student.findOne({ id }, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
 
   return result;
 };
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,13 +1,25 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'I am zod Error';
+  }
 
   return res.status(statusCode).json({
     success: false,
