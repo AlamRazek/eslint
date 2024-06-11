0b8fd1d109d69e2efddec2dbc3dd7f3ae0773dfa
AlamRazek <ohidul933@gmail.com>
2024-06-11 23:28:10 +0100

    router update and delete  created faculty

--- src/app/config/modules/Faculty/faculty.route.ts
+++ src/app/config/modules/Faculty/faculty.route.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import { FacultyControllers } from './faculty.controller';
+import { updateFacultyValidationSchema } from './faculty.validation';
+import validateRequest from '../../../middlewares/validateRequest';
 
 const router = express.Router();
 
 router.get('/:id', FacultyControllers.getSingleFaculty);
+
+router.patch(
+  '/:id',
+  validateRequest(updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty,
+);
+
+router.delete('/:id', FacultyControllers.deleteFaculty);
