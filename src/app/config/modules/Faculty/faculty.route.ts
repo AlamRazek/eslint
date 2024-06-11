b5cbf1d24f3fd587ffadc2f4292b5f8d5b84afd1
AlamRazek <ohidul933@gmail.com>
2024-06-11 23:27:24 +0100

    router get created faculty

--- src/app/config/modules/Faculty/faculty.route.ts
+++ src/app/config/modules/Faculty/faculty.route.ts
@@ -1,40 +1,5 @@
-import { Types } from 'mongoose';
+import express from 'express';
 
-export type TGender = 'male' | 'female' | 'other';
-export type TBloodGroup =
-  | 'A+'
-  | 'A-'
-  | 'B+'
-  | 'B-'
-  | 'AB+'
-  | 'AB-'
-  | 'O+'
-  | 'O-';
+const router = express.Router();
 
-export type TUserName = {
-  firstName: string;
-  middleName: string;
-  lastName: string;
-};
-
-export type TFaculty = {
-  id: string;
-  user: Types.ObjectId;
-  designation: string;
-  name: TUserName;
-  gender: TGender;
-  dateOfBirth?: Date;
-  email: string;
-  contactNo: string;
-  emergencyContactNo: string;
-  bloogGroup?: TBloodGroup;
-  presentAddress: string;
-  permanentAddress: string;
-  profileImg?: string;
-  academicDepartment: Types.ObjectId;
-  isDeleted: boolean;
-};
-
-export interface FacultyModel extends Model<TFaculty> {
-  isUserExists(id: string): Promise<TFaculty | null>;
-}
+router.get('/:id', FacultyControllers.getSingleFaculty);
