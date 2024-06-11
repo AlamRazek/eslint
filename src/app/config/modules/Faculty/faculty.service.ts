d0c15e511f5641a2503c6d63c9378dc4bfe41bee
AlamRazek <ohidul933@gmail.com>
2024-06-11 23:18:22 +0100

    in service delete faculty is created

--- src/app/config/modules/Faculty/faculty.service.ts
+++ src/app/config/modules/Faculty/faculty.service.ts
@@ -43,3 +43,43 @@ const updateFacultyIntoDB = async (id: string, payload: Partial<TFaculty>) => {
   });
   return result;
 };
+
+const deleteFacultyFromDB = async (id: string) => {
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const deletedFaculty = await Faculty.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedFaculty) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete faculty');
+    }
+
+    // get user _id from deletedFaculty
+    const userId = deletedFaculty.user;
+
+    const deletedUser = await User.findByIdAndUpdate(
+      userId,
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deletedFaculty;
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
+};
