e7517104eacdb700bb4f55971e33fcd346c3c5fb
AlamRazek <ohidul933@gmail.com>
2024-06-11 23:16:37 +0100

    in service update  faculty is created

--- src/app/config/modules/Faculty/faculty.service.ts
+++ src/app/config/modules/Faculty/faculty.service.ts
@@ -1,5 +1,6 @@
 import QueryBuilder from '../../../builder/QueryBuilder';
 import { FacultySearchableFields } from './faculty.constant';
+import { TFaculty } from './faculty.interface';
 import { Faculty } from './faculty.model';
 
 const getAllFacultiesFromDB = async (query: Record<string, unknown>) => {
@@ -20,5 +21,25 @@ const getAllFacultiesFromDB = async (query: Record<string, unknown>) => {
 const getSingleFacultyFromDB = async (id: string) => {
   const result = await Faculty.findById(id).populate('academicDepartment');
 
+  return result;
+};
+
+const updateFacultyIntoDB = async (id: string, payload: Partial<TFaculty>) => {
+  const { name, ...remainingFacultyData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingFacultyData,
+  };
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  const result = await Faculty.findByIdAndUpdate(id, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
   return result;
 };
