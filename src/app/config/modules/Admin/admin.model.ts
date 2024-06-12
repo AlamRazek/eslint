5c736fe6ba2a4deb64561374906abc911c1c6beb
AlamRazek <ohidul933@gmail.com>
2024-06-12 02:14:44 +0100

    admin schema is created all

--- src/app/config/modules/Admin/admin.model.ts
+++ src/app/config/modules/Admin/admin.model.ts
@@ -1,5 +1,6 @@
 import { Schema } from 'mongoose';
 import { AdminModel, TAdmin, TUserName } from './admin.interface';
+import { Gender } from '../Faculty/faculty.constant';
 
 const userNameSchema = new Schema<TUserName>({
   firstName: {
@@ -20,20 +21,70 @@ const userNameSchema = new Schema<TUserName>({
   },
 });
 
-const adminSchema = new Schema<TAdmin, AdminModel>({
-  id: {
-    type: String,
-    required: [true, 'ID is required'],
-    unique: true,
+const adminSchema = new Schema<TAdmin, AdminModel>(
+  {
+    id: {
+      type: String,
+      required: [true, 'ID is required'],
+      unique: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'User id is required'],
+      unique: true,
+      ref: 'User',
+    },
+    designation: {
+      type: String,
+      required: [true, 'Designation is required'],
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, 'Name is required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: Gender,
+        message: '{VAlUE} is not valid gender',
+      },
+      required: [true, 'Gender is required'],
+    },
+    dateOfBirth: { type: Date },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    contactNo: { type: String, required: [true, 'Contact number is required'] },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency contact number is required'],
+    },
+    bloogGroup: {
+      type: String,
+      enum: {
+        values: BloodGroup,
+        message: '{VALUE} is not a valid blood group',
+      },
+    },
+    presentAddress: {
+      type: String,
+      required: [true, 'Present address is required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent address is required'],
+    },
+    profileImg: { type: String },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    required: [true, 'User id is required'],
-    unique: true,
-    ref: 'User',
+  {
+    toJSON: {
+      virtuals: true,
+    },
   },
-  designation: {
-    type: String,
-    required: [true, 'Designation is required'],
-  },
-});
+);
