becd92480989468a6736890062031ba9dc01893b
AlamRazek <ohidul933@gmail.com>
2024-06-12 23:54:39 +0100

    admin update is created

--- src/app/config/modules/Admin/admin.service.ts
+++ src/app/config/modules/Admin/admin.service.ts
@@ -1,6 +1,7 @@
 import { Admin } from './admin.model';
 import QueryBuilder from '../../../builder/QueryBuilder';
 import { AdminSearchableFields } from './admin.constant';
+import { TAdmin } from './admin.interface';
 
 const getAllADminsFromDB = async (query: Record<string, unknown>) => {
   const adminQuery = new QueryBuilder(Admin.find(), query)
@@ -16,5 +17,25 @@ const getAllADminsFromDB = async (query: Record<string, unknown>) => {
 
 const getSingleAdmin = async (id: string) => {
   const result = await Admin.findById(id);
+  return result;
+};
+
+const updateAdminIntoDB = async (id: string, payload: Partial<TAdmin>) => {
+  const { name, ...remainingAdminData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingAdminData,
+  };
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  const result = await Admin.findByIdAndUpdate({ id }, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
   return result;
 };
