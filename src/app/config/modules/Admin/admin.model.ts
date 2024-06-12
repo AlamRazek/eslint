02afd4a5ef6f157f1e565049b8b48602120f6518
AlamRazek <ohidul933@gmail.com>
2024-06-12 02:09:21 +0100

    admin schema is created

---
+++ src/app/config/modules/Admin/admin.model.ts
@@ -0,0 +1,39 @@
+import { Schema } from 'mongoose';
+import { AdminModel, TAdmin, TUserName } from './admin.interface';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true,
+    maxLength: [20, 'Name can not be more than 20 characters'],
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, 'Last name is required'],
+    maxLength: [20, 'Name can not be more than 20 character'],
+  },
+});
+
+const adminSchema = new Schema<TAdmin, AdminModel>({
+  id: {
+    type: String,
+    required: [true, 'ID is required'],
+    unique: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'User id is required'],
+    unique: true,
+    ref: 'User',
+  },
+  designation: {
+    type: String,
+    required: [true, 'Designation is required'],
+  },
+});
