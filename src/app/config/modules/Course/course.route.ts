ba2c106dead88485a8656220143161e87dedbe56
AlamRazek <ohidul933@gmail.com>
2024-06-16 23:20:36 +0100

    course route is created

---
+++ src/app/config/modules/Course/course.route.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import validateRequest from '../../../middlewares/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    AcademicFacultyValidation.createAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get('/:FacultyId', AcademicFacultyControllers.getSingleAcademicFaculty);
+
+router.patch(
+  '/:facultyId',
+  validateRequest(
+    AcademicFacultyValidation.updateAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
+
+// router.get('/', StudentControllers.getAllStudents);
+
+// router.get('/:studentId', StudentControllers.getOneStudent);
+
+// router.delete('/:studentId', StudentControllers.deleteSingleStudent);
+
+export const AcademicFacultyRoutes = router;
