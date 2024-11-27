004011f1b20077a8830f29b12814feab8ce7e124
AlamRazek <ohidul933@gmail.com>
2024-11-27 20:19:31 +0000

    auth forgrt password

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -24,8 +24,20 @@ const refreshTokenValidationSchema = z.object({
   }),
 });
 
+const resetPasswordValidationSchema = z.object({
+  body: z.object({
+    id: z.string({
+      required_error: 'User id is required!',
+    }),
+    newPassword: z.string({
+      required_error: 'User password is required!',
+    }),
+  }),
+});
+
 export const AuthValidation = {
   loginValidationSchema,
   changePasswordValidationSchema,
   refreshTokenValidationSchema,
+  resetPasswordValidationSchema,
 };
