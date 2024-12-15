81425d44d1f6e51b87d26e574b41d108745b9aee
AlamRazek <ohidul933@gmail.com>
2024-12-15 21:45:44 +0000

    uploads file

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -3,8 +3,8 @@ import { USER_ROLE } from '../modules/user/user.constant';
 import { User } from '../modules/user/user.model';
 
 const superUser = {
-  id: '0001',
-  email: 'abedinforhan@gmail.com',
+  id: '0002',
+  email: 'abedinforhans@gmail.com',
   password: config.super_admin_password,
   needsPasswordChange: false,
   role: USER_ROLE.superAdmin,
